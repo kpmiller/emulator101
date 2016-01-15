36d83f4a18cdc6bb7827bf440c4194174202e631
Kent Miller <kpmiller@kent-rmbp.local>
2016-01-15 17:33:13 -0600

    first commit of part4 - touch

---
+++ iphonePart4-TouchHandling/PhoneInvaders/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PhoneInvaders
+//
+//  Created by Kent Miller on 11/16/11.
+//  Copyright (c) 2011 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iphonePart4-TouchHandling/PhoneInvaders/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PhoneInvaders
+//
+//  Created by Kent Miller on 11/16/11.
+//  Copyright (c) 2011 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
